62a1430373222fe03ec2b4e59b23dc939f55a67b
Shashank Baghel <theradcolor@gmail.com>
2021-09-28 23:44:00 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=a11052d98db2f2a61841f0c5ee84de4ca1b3e296, 28092021 build.
    GCC VERSION: aarch64-elf-gcc (GCC) 12.0.0 20210928 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=a11052d98db2f2a61841f0c5ee84de4ca1b3e296
    BUILDER COMMIT: f9b440467a6031d7567a6c72b1f6d8eb6bc2f4f9

--- aarch64-elf/lib/libstdc++.a-gdb.py
+++ aarch64-elf/lib/libstdc++.a-gdb.py
@@ -19,8 +19,8 @@ import gdb
 import os
 import os.path
 
-pythondir = '/drone/src/builder/aarch64-elf/share/gcc-12.0.0/python'
-libdir = '/drone/src/builder/aarch64-elf/aarch64-elf/lib'
+pythondir = '/home/runner/work/build-tools-gcc/build-tools-gcc/aarch64-elf/share/gcc-12.0.0/python'
+libdir = '/home/runner/work/build-tools-gcc/build-tools-gcc/aarch64-elf/aarch64-elf/lib'
 
 # This file might be loaded when there is no current objfile.  This
 # can happen if the user loads it manually.  In this case we don't
--- lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
+++ lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=aarch64-elf --prefix=/drone/src/builder/aarch64-elf --disable-nls --disable-shared --with-newlib --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=aarch64-elf --prefix=/home/runner/work/build-tools-gcc/build-tools-gcc/aarch64-elf --disable-nls --disable-shared --with-newlib --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
 static const char thread_model[] = "single";
 
 static const struct {
